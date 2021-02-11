9a28b06fba1a7de2d4120b7a9f8b70d136a503e8
slee7 <slee@catalina7ventures.com>
2021-02-10 22:57:11 -0800

    uninstalled materialUI and added ChakraUI

---
+++ src/app/components/schedule/Schedule.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function Schedule() {
+  return (
+    <Box w="90%" bgColor="white">
+      <Heading>Schedule</Heading>
+    </Box>
+  );
+}
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Box } from "@material-ui/core";
 import React from "react";
 import Calendar from "../components/calendar/Calendar";
 import Navbar from "../components/navbar/Navbar";
+import Schedule from "../components/schedule/Schedule";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Box width="100%" display="flex" justifyContent="center" marginTop="20px">
         <Calendar />
       </Box>
+      <Schedule />
     </Box>
   );
 }
